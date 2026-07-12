956d639bd7823034b5f7a020b5ece955951cf8b0
swarupa-devops <swarupaandra15@gmail.com>
2026-07-12 21:40:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.4.0"
+      version = "~>6.4.0"
     }
   }
 }
-
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias  = "mumbai"
 }
